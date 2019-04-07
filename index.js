972b258044791db3865401e7c28667e846fd7d2a
David Dozsa <dozsalevi@gmail.com>
2019-04-07 09:09:37 +0100

    messages endpoint with get and post, connect to mongodb

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(helmet())
 app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html')
     res.send(200, `
-        <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
+        <h1>Magic Mirror - Crazy Code</h1>
     `)
 })
 module.exports = app
